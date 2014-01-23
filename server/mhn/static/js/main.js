ab25c29c69e192581576fac5ff917719b103c878
Sergio Pulgarin <sergio.pulgarin@gmail.com>
2014-01-23 16:27:55 -0500

    Added Rules page to ui blueprint.
    Can edit message and enable/disable rules.

--- server/mhn/static/js/main.js
+++ server/mhn/static/js/main.js
@@ -23,4 +23,55 @@ $(document).ready(function() {
             });
         });
     }
+
+    if ($('#rule-table').length >= 1) {
+        var requestChange = function(inputObj, data, success, error) {
+            var ruleId = inputObj.attr('data-rule-id');
+
+            inputObj.attr('enabled', false);
+            console.log(data);
+            console.log('/api/rule/' + ruleId + '/');
+            $.ajax({
+                type: 'PUT',
+                url: '/api/rule/' + ruleId + '/',
+                data: JSON.stringify(data),
+                success: success,
+                contentType: 'application/json',
+                error: error,
+                always: function(resp) {
+                    inputObj.attr('enabled', true);
+                }
+            });
+        };
+        $('.checkbox').click(function() {
+            var checkbox = $(this);
+            var isChecked = checkbox.is(':checked');
+
+            requestChange(
+                checkbox,
+                {is_active: isChecked},  // Data
+                function() {},           // Success
+                function() {             // Error
+                    // Reverses the state.
+                    checkbox.prop({checked: !isChecked});
+                }
+            );
+        });
+        $('.text-edit').focusout(function() {
+            var input = $(this);
+            var fieldName = input.attr('data-field-name');
+            var data = new Object();
+
+            data[fieldName] = input.val();
+            requestChange(
+                input,
+                data,                    // Data
+                function() {},           // Success
+                function() {             // Error
+                    // Reverses the state.
+                    alert('Could not save changes.');
+                }
+            );
+        });
+    }
 });
