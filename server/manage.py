a22e7dc1221b1ef55daacb12064f94f1396083bb
Sergio Pulgarin <sergio.pulgarin@gmail.com>
2014-02-01 11:26:51 -0500

    Added flask-migrate module and manage.py script.

---
+++ server/manage.py
@@ -0,0 +1,12 @@
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+from mhn import create_app, db
+
+
+if __name__ == '__main__':
+    app = create_app()
+    migrate = Migrate(app, db)
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
+    manager.run()
