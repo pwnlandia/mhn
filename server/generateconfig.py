33a02473f7a6180df7ed565250576a4035f65a93
Sergio Pulgarin <sergio.pulgarin@gmail.com>
2014-03-15 11:40:49 -0500

    Refactored config generator into function.
    log file path now has a default value.

--- server/generateconfig.py
+++ server/generateconfig.py
@@ -11,9 +11,11 @@ from socket import gethostbyname, gethostname
 
 el = string.ascii_letters + string.digits
 rand_str = lambda n: ''.join(choice(el) for _ in range(n))
+default_base_url = 'http://{}'.format(gethostbyname(gethostname()))
+default_log_path = 'mhn.log'
 
 
-if __name__ == '__main__':
+def generate_config():
     localconfig = {}
     localconfig['SECRET_KEY'] = rand_str(32)
     localconfig['DEPLOY_KEY'] = rand_str(8)
@@ -23,18 +25,23 @@ if __name__ == '__main__':
     localconfig['DEBUG'] = 'y' == debug
     localconfig['SUPERUSER_EMAIL'] = raw_input('Superuser email: ')
     localconfig['SUPERUSER_PASSWORD'] = getpass('Superuser password: ')
-    guess_base_url = 'http://{}'.format(gethostbyname(gethostname()))
-    server_base_url = raw_input('Server base url ["{}"]: '.format(guess_base_url))
+    server_base_url = raw_input('Server base url ["{}"]: '.format(default_base_url))
     if server_base_url.endswith('/'):
         server_base_url = server_base_url[:-1]
-    server_base_url = server_base_url if server_base_url.strip() else guess_base_url
+    server_base_url = server_base_url if server_base_url.strip() else default_base_url
     localconfig['SERVER_BASE_URL'] = server_base_url
-    localconfig['LOG_FILE_PATH'] = raw_input('Path for log file: ')
+    log_file_path = raw_input('Path for log file ["{}"]: '.format(default_log_path))
+    log_file_path = log_file_path if log_file_path else default_log_path
+    localconfig['LOG_FILE_PATH'] = log_file_path
     with open('config.py.template', 'r') as templfile,\
-         open('config.py.test', 'w') as confile:
+         open('config.py', 'w') as confile:
         templ = templfile.read()
         for key, setting in localconfig.iteritems():
             if isinstance(setting, bool):
                 setting = 'True' if setting else 'False'
             templ = templ.replace('{{' + key + '}}', setting)
         confile.write(templ)
+
+
+if __name__ == '__main__':
+    generate_config()
