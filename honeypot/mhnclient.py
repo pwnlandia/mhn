8924d8df92451062619e1854c96b5893e658fbc9
Sergio Pulgarin <sergio.pulgarin@gmail.com>
2014-03-22 01:13:55 -0500

    Updated snort alert parser to include full signature.

--- honeypot/mhnclient.py
+++ honeypot/mhnclient.py
@@ -231,22 +231,23 @@ class Alert(object):
 
     def __init__(self, sensor_uuid, *args):
         try:
-            assert len(args) == 7
+            assert len(args) == 8
         except:
             raise ValueError("Unexpected number of attributes.")
         else:
             self.header = args[0]
-            self.classification = args[1]
-            self.priority = args[2]
+            self.signature = args[1]
+            self.classification = args[2]
+            self.priority = args[3]
             # Alert logs don't include year. Creating a datime object
             # with current year.
-            date = datetime.strptime(args[3], '%m/%d-%H:%M:%S.%f')
+            date = datetime.strptime(args[4], '%m/%d-%H:%M:%S.%f')
             self.date = datetime(
                     datetime.now().year, date.month, date.day,
                     date.hour, date.minute, date.second, date.microsecond)
-            self.source_ip = args[4]
-            self.destination_ip = args[5]
-            self.destination_port = args[6]
+            self.source_ip = args[5]
+            self.destination_ip = args[6]
+            self.destination_port = args[7]
             self.sensor = sensor_uuid
 
     def __repr__(self):
@@ -271,8 +272,7 @@ class Alert(object):
         integer = pyp.Word(pyp.nums)
         ip_addr = pyp.Combine(integer + '.' + integer+ '.' + integer + '.' + integer)
         port = pyp.Suppress(':') + integer
-
-        # Defining pyparsing objects from expected format:
+# Defining pyparsing objects from expected format:
         #
         #    [**] [1:160:2] COMMUNITY SIP TCP/IP message flooding directed to SIP proxy [**]
         #    [Classification: Attempted Denial of Service] [Priority: 2]
@@ -284,21 +284,26 @@ class Alert(object):
         # Works with Snort version 2.9.2 IPv6 GRE (Build 78)
 
         header = (
-        pyp.Suppress("[**] [")
-        + pyp.Combine(integer + ":" + integer + ":" + integer)
-        + pyp.Suppress(pyp.SkipTo("[**]", include=True))
+            pyp.Suppress("[**] [")
+            + pyp.Combine(integer + ":" + integer + ":" + integer)
+            + pyp.Suppress("]")
+        )
+        signature = (
+            pyp.Combine(pyp.SkipTo("[**]", include=False))
         )
         classif = (
-            pyp.Suppress(pyp.Optional(pyp.Literal("[Classification:")))
+            pyp.Suppress("[**]")
+            + pyp.Suppress(pyp.Optional(pyp.Literal("[Classification:")))
             + pyp.Regex("[^]]*") + pyp.Suppress(']')
         )
         pri = pyp.Suppress("[Priority:") + integer + pyp.Suppress("]")
         date = pyp.Combine(
-            integer + "/" + integer + '-' + integer + ':' + integer + ':' + integer + '.' + integer)
+            integer + "/" + integer + '-' + integer + ':' + integer + ':' + integer + '.' + integer
+        )
         src_ip = ip_addr + pyp.Suppress(port + "->")
         dest_ip = ip_addr
         dest_port = port
-        bnf = header + classif + pri + date + src_ip + dest_ip + dest_port
+        bnf = header + signature + classif + pri + date + src_ip + dest_ip + dest_port
 
         alerts = []
         with open(logfile) as snort_logfile:
@@ -314,13 +319,13 @@ class Alert(object):
                             # the delta between local time and UTC and uses it to convert
                             # the logged time to UTC. Additional time formatting  makes
                             # sure the previous code doesn't break.
-                            date = datetime.strptime(fields[0][3], '%m/%d-%H:%M:%S.%f')
+                            date = datetime.strptime(fields[0][4], '%m/%d-%H:%M:%S.%f')
                             date = datetime(
                                datetime.now().year, date.month, date.day,
                                date.hour, date.minute, date.second, date.microsecond)
                             toutc = datetime.utcnow() - datetime.now()
                             date = date + toutc
-                            fields[0][3] = date.strftime('%m/%d-%H:%M:%S.%f')
+                            fields[0][4] = date.strftime('%m/%d-%H:%M:%S.%f')
                         alert = cls(sensor_uuid, *fields[0])
                         if (mindate and alert.date > mindate) or not mindate:
                             # If mindate parameter is passed, only newer
