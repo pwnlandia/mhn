27062959a10c92bdaa468f7229b2f54822440ef2
Sergio Pulgarin <sergio.pulgarin@gmail.com>
2014-04-29 14:59:15 -0700

    Moving APIModel to different module

--- server/mhn/api/models.py
+++ server/mhn/api/models.py
@@ -4,42 +4,8 @@ from flask.helpers import url_for
 from sqlalchemy import UniqueConstraint, func
 
 from mhn import db
+from mhn.api import APIModel
 from mhn.auth.models import User
-
-
-class APIModel(object):
-    @classmethod
-    def fields(cls):
-        return cls.all_fields.keys()
-
-    @classmethod
-    def editable_fields(cls):
-        return cls._make_field_list('editable')
-
-    @classmethod
-    def required_fields(cls):
-        return cls._make_field_list('required')
-
-    @classmethod
-    def _make_field_list(cls, prop):
-        """
-        Returns a list of field names that have the property
-        `prop` in the `all_fields` dictionary defined at
-        class level.
-        """
-        return [f for f, e in cls.all_fields.items() if e.get(prop, False)]
-
-    @classmethod
-    def check_required(cls, payload):
-        """
-        Returns a list of required fields that are
-        missing from the dictionary object `payload`.
-        """
-        missing = []
-        for field in cls.required_fields():
-            if (field not in payload) or payload.get(field) == '':
-                missing.append(field)
-        return missing
 
 
 class Sensor(db.Model, APIModel):
