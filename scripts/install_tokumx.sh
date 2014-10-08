6c261f2de2459e2475ac3e9cc26e21c2a0c7c655
Jason Trost <jason.trost@gmail.com>
2014-10-08 12:35:33 -0400

    WIP support for replacing mongo with tokumx

--- scripts/install_tokumx.sh
+++ scripts/install_tokumx.sh
@@ -2,4 +2,5 @@
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-key 505A7412
 echo "deb [arch=amd64] http://s3.amazonaws.com/tokumx-debs $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/tokumx.list
+apt-get update
 apt-get -y install tokumx
