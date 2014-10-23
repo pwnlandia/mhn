da7a32b52dc188f4365ffc15fd6b2ce27bd44adb
Jason Trost <jason.trost@gmail.com>
2014-10-23 17:49:23 -0400

    converted p0f deploy to use the new p0f that uses libhpfeeds

--- scripts/deploy_p0f.sh
+++ scripts/deploy_p0f.sh
@@ -19,39 +19,20 @@ chmod 755 registration.sh
 . ./registration.sh $server_url $deploy_key "p0f"
 
 apt-get update
-apt-get -y install git python-pip supervisor libpcap-dev libjansson-dev python-dev gcc
-pip install virtualenv
+apt-get -y install git supervisor libpcap-dev libjansson-dev gcc
 
 # install p0f
 cd /opt
 git clone https://github.com/threatstream/p0f.git
 cd p0f
-git checkout hpfeeds-publishing
+git checkout origin/hpfeeds
 ./build.sh
-useradd -d /var/empty/p0f -M -r -s /bin/nologin p0f-user
+useradd -d /var/empty/p0f -M -r -s /bin/nologin p0f-user || true
 mkdir -p -m 755 /var/empty/p0f
-
-cd hpfeeds
-virtualenv env
-. env/bin/activate
-
-pip install -e git+https://github.com/threatstream/hpfeeds.git#egg=hpfeeds-dev
-pip install ujson
-pip install cachetools
-
-cat > p0f_hpfeeds.json <<EOF
-{
-	"HOST":   "$HPF_HOST",
-	"PORT":   $HPF_PORT,
-	"IDENT":  "$HPF_IDENT",
-	"SECRET": "$HPF_SECRET",
-    "PUB_CHANNEL": "p0f.events"
-}
-EOF
 
 cat > /etc/supervisor/conf.d/p0f.conf <<EOF
 [program:p0f]
-command=/opt/p0f/p0f_wrapper.sh 
+command=/opt/p0f/p0f_wrapper.sh
 directory=/opt/p0f
 stdout_logfile=/var/log/p0f.out         
 stderr_logfile=/var/log/p0f.err          
@@ -59,24 +40,7 @@ autostart=true
 autorestart=true
 redirect_stderr=true
 stopsignal=TERM
+environment=HPFEEDS_HOST="$HPF_HOST",HPFEEDS_PORT="$HPF_PORT",HPFEEDS_CHANNEL="p0f.events",HPFEEDS_IDENT="$HPF_IDENT",HPFEEDS_SECRET="$HPF_SECRET"
 EOF
 
 supervisorctl update
-
-cat > /etc/logrotate.d/p0f <<EOF
-/var/log/p0f.json.log {
-        daily
-        copy
-        rotate 5
-        compress
-        delaycompress
-        notifempty
-        create 700 root root
-        sharedscripts
-        compresscmd /bin/bzip2
-        compressext .bz2
-        postrotate
-                /usr/bin/supervisorctl restart p0f
-        endscript
-}
-EOF
