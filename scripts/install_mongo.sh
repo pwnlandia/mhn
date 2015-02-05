665dcf42e93dbceb23d70308406597a5be46c4be
John <root@dummy>
2015-02-05 15:07:53 +0800

    Modify mirro from 'ubuntu-upstart' to 'debian-sysvinit'

--- scripts/install_mongo.sh
+++ scripts/install_mongo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
