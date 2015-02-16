ddfec03bb000571a38a2e293ce9e00111aeb5128
hink <hink@blackbird.local>
2015-02-16 09:45:38 -0600

    Made unattended script directory independent

--- scripts/install_mhnserver_unattended.sh
+++ scripts/install_mhnserver_unattended.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-if test -f "../server/config.py"; then
-    ./install_mhnserver.sh
+scripts_directory="$(dirname $(readlink -f ${BASH_SOURCE}))"
+mhn_directory="$(dirname ${scripts_directory})"
+
+if test -f "${mhn_directory}/server/config.py"; then
+    "${mhn_directory}/scripts/install_mhnserver.sh"
 else
-    echo "../server/config.py does not exist. Please create the configuration file first using the generateconfig.py script."
+    echo "${mhn_directory}/server/config.py does not exist. Please create the configuration file first using the generateconfig.py script."
     exit 1
 fi
