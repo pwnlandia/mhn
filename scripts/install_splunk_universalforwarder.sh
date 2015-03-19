6f4df3ce3cf8f045ce14bef63667b752e874bca1
Jason Trost <jason.trost@gmail.com>
2015-03-19 16:29:35 -0400

    got it working

--- scripts/install_splunk_universalforwarder.sh
+++ scripts/install_splunk_universalforwarder.sh
@@ -2,17 +2,21 @@
 
 # ref: http://docs.splunk.com/Documentation/Splunk/6.2.2/Forwarding/Deployanixdfmanually#Install_the_universal_forwarder
 
+if [ "$(whoami)" != "root" ]
+then
+	echo "You must be root to run this script"
+	exit 1
+fi
+
 set -e
 set -x
 
 SPLUNK_HOST="$1"
 SPLUNK_PORT="$2"
-SPLUNK_USER="$3"
-SPLUNK_PASS="$4"
 
-if [ -z "$SPLUNK_HOST" -o -z "$SPLUNK_PORT" -o -z "$SPLUNK_USER" -o -z "$SPLUNK_PASS" ]
+if [ -z "$SPLUNK_HOST" -o -z "$SPLUNK_PORT" ]
 then
-	echo "Usage: $0 <SPLUNK_HOST> <SPLUNK_PORT> <SPLUNK_USER> <SPLUNK_PASS>"
+	echo "Usage: $0 <SPLUNK_HOST> <SPLUNK_PORT>"
 	exit 1
 fi
 
@@ -21,16 +25,17 @@ cd /tmp/
 FILENAME="splunkforwarder-6.2.2-255606-linux-2.6-amd64.deb"
 rm -f "${FILENAME}"
 wget -O "${FILENAME}" "http://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=Linux&version=6.2.2&product=universalforwarder&filename=${FILENAME}&wget=true"
-sudo dpkg -i "${FILENAME}"
+dpkg -i "${FILENAME}"
 
-export SPLUNK_HOME="/opt/splunk"
-export PATH="$PATH:$SPLUNK_HOME/bin"
-splunk start --accept-license
-splunk enable boot-start
+SPLUNK="/opt/splunkforwarder/bin/splunk"
 
-splunk add forward-server "${SPLUNK_HOST}:${SPLUNK_PORT}" -auth "${SPLUNK_USER}:${SPLUNK_PASS}"
+$SPLUNK start --accept-license
+$SPLUNK enable boot-start
+$SPLUNK add forward-server "${SPLUNK_HOST}:${SPLUNK_PORT}" -auth admin:changeme
+$SPLUNK add monitor /var/log/mhn/
 
-splunk add monitor /var/log/mhn/
+echo "splunkforwarder installed and configured to monitor /var/log/mhn/"
+echo "It is highly recommended that you change the password for this local splunk install"
 
 # might want to do this...
 # splunk add forward-server <host>:<port> -ssl-cert-path /path/ssl.crt -ssl-root-ca-path /path/ca.crt -ssl-password <password>
