39625247635eb67b30dd504eaa594e76c5983a2e
nedshawa <ned.shawa@gmail.com>
2017-05-30 17:03:47 +1000

    Updated Cowrie deploy to work on the latest
    many changes happened on cowrie, no more start.sh and some changes occured in the cfg file needed to be changed.

--- scripts/deploy_cowrie.sh
+++ scripts/deploy_cowrie.sh
@@ -37,16 +37,17 @@ useradd -d /home/cowrie -s /bin/bash -m cowrie -g users
 cd /opt
 git clone https://github.com/micheloosterhof/cowrie.git cowrie
 cd cowrie
-virtualenv env
-source env/bin/activate
+virtualenv cowrie-env
+source cowrie-env/bin/activate
 # without the following, i get this error:
 # Could not find a version that satisfies the requirement csirtgsdk (from -r requirements.txt (line 10)) (from versions: 0.0.0a5, 0.0.0a6, 0.0.0a5.linux-x86_64, 0.0.0a6.linux-x86_64, 0.0.0a3)
 pip install csirtgsdk==0.0.0a6
 pip install -r requirements.txt 
 
 cp cowrie.cfg.dist cowrie.cfg
-sed -i 's/hostname = svr04/hostname = server/g' cowrie.cfg
+sed -i 's/hostname = svr04/hostname = srv-web/g' cowrie.cfg
 sed -i 's/#listen_port = 2222/listen_port = 22/g' cowrie.cfg
+sed -i 's/#listen_endpoints = tcp:2222:interface=0.0.0.0/listen_endpoints = tcp:22:interface=0.0.0.0/g' cowrie.cfg
 sed -i 's/ssh_version_string = SSH-2.0-OpenSSH_6.0p1 Debian-4+deb7u2/ssh_version_string = SSH-2.0-OpenSSH_6.7p1 Ubuntu-5ubuntu1.3/g' cowrie.cfg
 sed -i 's/#\[output_hpfeeds\]/[output_hpfeeds]/g' cowrie.cfg
 sed -i "s/#server = hpfeeds.mysite.org/server = $HPF_HOST/g" cowrie.cfg
@@ -60,13 +61,13 @@ touch /etc/authbind/byport/22
 chown cowrie /etc/authbind/byport/22
 chmod 770 /etc/authbind/byport/22
 
-sed -i 's/AUTHBIND_ENABLED=no/AUTHBIND_ENABLED=yes/' start.sh
-sed -i 's/DAEMONIZE=""/DAEMONIZE="-n"/' start.sh
+sed -i 's/AUTHBIND_ENABLED=no/AUTHBIND_ENABLED=yes/' bin/cowrie
+sed -i 's/DAEMONIZE=""/DAEMONIZE="-n"/' bin/cowrie
 
 # Config for supervisor
 cat > /etc/supervisor/conf.d/cowrie.conf <<EOF
 [program:cowrie]
-command=/opt/cowrie/start.sh env
+command=/opt/cowrie/bin/cowrie start
 directory=/opt/cowrie
 stdout_logfile=/opt/cowrie/log/cowrie.out
 stderr_logfile=/opt/cowrie/log/cowrie.err
