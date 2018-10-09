dab84db88fb5708f79d1ac17e5ea46bf465558ae
Brady Sullivan <brady@bsull.com>
2018-10-09 03:48:57 -0700

    Update ubuntu 18 mongodb install.

--- scripts/install_mongodb_ub18.sh
+++ scripts/install_mongodb_ub18.sh
@@ -5,7 +5,5 @@
 set -e
 set -x
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
-apt-get update
-apt-get install -y mongodb-org
+apt update
+apt install -y mongodb
