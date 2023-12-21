90c62f967cf1a801ddbb5dc59f641148fd841060
William Harding <wrharding@protonmail.com>
2023-12-21 18:03:30 +0000

    Convert to Python 3 syntax and lib

--- server/generateconfig.py
+++ server/generateconfig.py
@@ -9,7 +9,7 @@ import os.path
 from random import choice
 import string
 import sys
-from urllib2 import urlopen
+from urllib.request import urlopen
 
 import argparse
 
@@ -19,9 +19,9 @@ rand_str = lambda n: ''.join(choice(el) for _ in range(n))
 
 def get_pub_ip():
     sock = urlopen('http://icanhazip.com/')
-    ip = sock.read().rstrip()
+    ip = sock.read().strip()
     sock.close()
-    return ip
+    return ip.decode('utf-8')
 
 def generate_config():
     # Check if config file already exists
@@ -103,14 +103,14 @@ def generate_config():
         log_file_path = args.log_file_path
     else:
         # Collect values from user
-        debug = raw_input('Do you wish to run in Debug mode?: y/n ')
+        debug = input('Do you wish to run in Debug mode?: y/n ')
         while debug not in ['y', 'n']:
-            debug = raw_input('Please y or n ')
+            debug = input('Please y or n ')
         debug = True if debug == 'y' else False
 
-        email = raw_input('Superuser email: ')
+        email = input('Superuser email: ')
         while '@' not in email:
-            email = raw_input('Superuser email (must be valid): ')
+            email = input('Superuser email (must be valid): ')
 
         while True:
             password = getpass('Superuser password: ')
@@ -124,36 +124,36 @@ def generate_config():
             if password == password2:
                 break
             else:
-                print "Passwords did not match. Try again"
+                print("Passwords did not match. Try again")
 
-        server_base_url = raw_input('Server base url ["{}"]: '.format(default_base_url))
+        server_base_url = input('Server base url ["{}"]: '.format(default_base_url))
         if server_base_url.endswith('/'):
             server_base_url = server_base_url[:-1]
 
         server_base_url = server_base_url if server_base_url.strip() else default_base_url
 
-	default_honeymap_url = '{}:3000'.format(server_base_url)
-        honeymap_url = raw_input('Honeymap url ["{}"]: '.format(default_honeymap_url))
+        default_honeymap_url = '{}:3000'.format(server_base_url)
+        honeymap_url = input('Honeymap url ["{}"]: '.format(default_honeymap_url))
         if honeymap_url.endswith('/'):
             honeymap_url = honeymap_url[:-1]
 
-        mail_server = raw_input('Mail server address ["localhost"]: ')
-        mail_port = raw_input('Mail server port [25]: ')
+        mail_server = input('Mail server address ["localhost"]: ')
+        mail_port = input('Mail server port [25]: ')
 
-        mail_tls = raw_input('Use TLS for email?: y/n ')
+        mail_tls = input('Use TLS for email?: y/n ')
         while mail_tls not in ['y', 'n']:
-            mail_tls = raw_input('Please y or n ')
+            mail_tls = input('Please y or n ')
 
-        mail_ssl = raw_input('Use SSL for email?: y/n ')
+        mail_ssl = input('Use SSL for email?: y/n ')
         while mail_ssl not in ['y', 'n']:
-            mail_ssl = raw_input('Please y or n ')
+            mail_ssl = input('Please y or n ')
 
-        mail_username = raw_input('Mail server username [""]: ')
+        mail_username = input('Mail server username [""]: ')
         mail_password = getpass('Mail server password [""]: ')
 
-        default_mail_sender = raw_input('Mail default sender [""]: ')
+        default_mail_sender = input('Mail default sender [""]: ')
 
-        log_file_path = raw_input('Path for log file ["{}"]: '.format(default_log_path))
+        log_file_path = input('Path for log file ["{}"]: '.format(default_log_path))
 
     honeymap_url = honeymap_url if honeymap_url.strip() else default_honeymap_url
     log_file_path = log_file_path if log_file_path else default_log_path
@@ -175,7 +175,7 @@ def generate_config():
     with open('config.py.template', 'r') as templfile,\
          open('config.py', 'w') as confile:
         templ = templfile.read()
-        for key, setting in localconfig.iteritems():
+        for key, setting in iter(localconfig.items()):
             templ = templ.replace('{{' + key + '}}', str(setting))
         confile.write(templ)
 
--- server/mhn/__init__.py
+++ server/mhn/__init__.py
@@ -1,4 +1,4 @@
-from urlparse import urljoin
+from urllib.parse import urljoin
 
 from flask import Flask, request, jsonify, abort, url_for, session
 from flask_sqlalchemy import SQLAlchemy
