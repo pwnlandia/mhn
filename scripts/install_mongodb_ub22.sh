1df8cb7b5926cadb9f510bf916c669b63e2a592e
NoL0317 <43594759+janus03171014@users.noreply.github.com>
2024-03-19 10:10:28 +0800

    Update install_mongodb_ub22.sh
    Downgrade due to mhn execution problem.
    Mongodb version changed from 7.0 to 4.4.

--- scripts/install_mongodb_ub22.sh
+++ scripts/install_mongodb_ub22.sh
@@ -8,11 +8,11 @@ set -x
 sudo apt-get install gnupg curl -y
 
 if [ ! -f "/usr/share/keyrings/mongodb-server-7.0.gpg" ]; then
-  curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-  sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-  --dearmor
+  curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg \
+   --dearmor
 
-  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 fi
 
 sudo apt-get update
@@ -20,9 +20,8 @@ sudo apt-get update
 sudo apt-get install -y mongodb-org
 
 echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-database hold" | sudo dpkg --set-selections
 echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
 echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
