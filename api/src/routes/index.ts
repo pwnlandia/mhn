66f19f8218a7402cf35326deebd84f46a2d40011
Brady Sullivan <brady@bsull.com>
2024-11-28 13:49:40 -0800

    Refactor root and index route files into one file.

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,15 +1,40 @@
 // src/routes/index.ts
 import { FastifyInstance } from 'fastify';
+import sensible from '@fastify/sensible';
+import { helloHandler } from '../handlers/handlers';
 import userRoutes from './api/user.route';
 import errorHandler from '../plugins/errorHandler';
 
-export default async function apiRoutes(fastify: FastifyInstance) {
-  await fastify.register(async (fastify) => {
-    // Register error handler first within the same scope as routes
-    await fastify.register(errorHandler);
+export default async function routes(fastify: FastifyInstance) {
+  fastify.register(sensible);
 
-    // Register routes in same scope as error handler
+  // Root routes
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    handler: async function () {
+      fastify.log.info('GET / route hit');
+      return { root: true };
+    },
+  });
+
+  fastify.route({
+    method: 'GET',
+    url: '/error',
+    handler: async function () {
+      throw new Error('Test error');
+    },
+  });
+
+  fastify.route({
+    method: 'GET',
+    url: '/hello',
+    handler: helloHandler,
+  });
+
+  // API routes with error handler
+  await fastify.register(async (fastify) => {
+    await fastify.register(errorHandler);
     await fastify.register(userRoutes);
-    // Add more API route modules here
   });
 }
