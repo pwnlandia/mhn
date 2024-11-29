83f3894d9edf7d70b6df57323d2873575f7b1edc
Brady Sullivan <brady@bsull.com>
2024-11-28 22:12:04 -0800

    Remove node modules and dist files

--- api/dist/handlers/handlers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// handlers/handlers.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloHandler = helloHandler;
-async function helloHandler() {
-    return { message: 'Hello, world!' };
-}
